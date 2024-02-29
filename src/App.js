34accc7f663e7d9964451573c5b35f0852bec1e3
sanjay <sanjay139747@gmail.com>
2024-02-29 18:03:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import './index.css';
+import BookCreate from './components/BookCreate';
+
 
 function App() {
+  let [books,setBooks] = useState([]);
+  const createBook = (title) => {
+    console.log(title)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BookCreate onCreate = {createBook} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/BookCreate.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function BookCreate({onCreate}) {
+
+  let [title,setTitle] = React.useState("");
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreate(title);
+
+  }
+
+  return (
+    <div className='book-create'>
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input className='input' value={title} onChange = {handleChange}/>
+        <input className='button' type = "submit" value = {"Submit"}/>
+      </form>
+    </div>
+  )
+}
+
+export default BookCreate;
